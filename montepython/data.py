d8645216fac59b3737d92149856138a82923f371
Florian Niedermann <florian.niedermann@gmail.com>
2020-03-27 23:26:38 +0100

    run165

--- montepython/data.py
+++ montepython/data.py
@@ -831,6 +831,28 @@ class Data(object):
                 self.cosmo_arguments['Omega_EDE2'] = frac_EDE * mphi**2 / (h * 10**5 / (2.99792458*10**8) )**2 * trigger**2
 
                 del self.cosmo_arguments[elem]
+
+            elif elem == 'log10f':
+                
+                frac_EDE = 10**self.cosmo_arguments[elem]
+                h = self.cosmo_arguments['h']
+                try: 
+                    mphi = 10**self.cosmo_arguments['log10mass']
+                except:
+                    mphi = self.cosmo_arguments['EDE2_clock_mass']
+                try:
+                    trigger = self.cosmo_arguments['trigger']           
+                except:
+                    try:
+                        trigger = self.cosmo_arguments['Bubble_trigger_H_over_m']
+                    except:
+                        trigger = 1.
+
+                self.cosmo_arguments['Omega_EDE2'] = frac_EDE * mphi**2 / (h * 10**5 / (2.99792458*10**8) )**2 * trigger**2
+
+                del self.cosmo_arguments[elem]
+
+
                 
             elif elem == 'log10R_phi':
                 self.cosmo_arguments['EDE2_clock_pert_ini'] = 10**(-self.cosmo_arguments[elem])*self.cosmo_arguments['EDE2_clock_ini']
