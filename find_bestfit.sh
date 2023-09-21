279c45ea2a078c17d8411a66485147c8bdd2e7c9
Florian Niedermann <florian@MBP-Florian-Niedermann.local>
2023-09-21 17:23:56 +0200

    run341

--- find_bestfit.sh
+++ find_bestfit.sh
@@ -1,7 +1,7 @@
 for i in {1..30}
 do
 echo "Iteration: $i \n"
-    mpirun -np 8 python montepython/MontePython.py run -o ../output/$1/ -f $3 -b ../output/$1/$1.bestfit -N $2 -c ../output/run$1/$1.covmat --silent > ../output/$1/term.txt
+    mpirun -np 8 python montepython/MontePython.py run -o ../output/$1/ -f $3 -b ../output/$1/$1.bestfit -N $2 -c ../output/$1/$1.covmat --silent > ../output/$1/term.txt
 
     python montepython/MontePython.py info ../output/$1 --noplot --silent --minimal
 done
