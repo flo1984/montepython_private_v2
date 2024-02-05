2b99d584b2a5869efd37f6c9b5dcee71fe47282e
Florian Niedermann <florian@nordita-dyn-235.nordita.su.se>
2024-02-05 17:38:17 +0100

    run381

---
+++ montepython/likelihoods/BBN_Neff/__init__.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+from montepython.likelihood_class import Likelihood
+import scipy.constants as conts
+
+class BBN_Neff(Likelihood):
+
+    # initialization routine
+
+    def __init__(self, path, data, command_line):
+
+        Likelihood.__init__(self, path, data, command_line)
+
+
+        # end of initialization
+
+    # compute likelihood
+
+    def loglkl(self, cosmo, data):
+
+        chi2 = 0.
+
+        theo = cosmo.delta_N_eff_UV
+
+        chi2 += ((theo - self.Neff) / self.error) ** 2
+
+        # return ln(L)
+        lkl = - 0.5 * chi2
+
+        return lkl
